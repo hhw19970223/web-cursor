20619ffdaf6b6c5013976b8b7194b0d44d31bce2
hhw <1171604288@qq.com>
2025-11-28 16:59:06 +0800

    bug

--- src/app/(DashboardLayout)/page.tsx
+++ src/app/(DashboardLayout)/page.tsx
@@ -21,15 +21,6 @@ const page = () => {
   return (
     <div className="px-4 pb-4 h-full">
       <div className="bg-lightgray rounded-page h-full w-full pt-20">
-        <div className="container mb-6">
-          <Input
-            type="text"
-            placeholder="请输入内容..."
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            className="max-w-md"
-          />
-        </div>
         <div className="container grid grid-cols-4 gap-4">
           {list.map((item, index) => (
             <div
